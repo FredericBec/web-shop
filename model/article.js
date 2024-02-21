37d130c938985c1913b175b9e1fb518eec7b2f57
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-21 10:26:57 +0100

    Add model article, category and cart

---
+++ model/article.js
@@ -0,0 +1,9 @@
+class Article{
+    constructor(name, price, idCategory){
+        this.name = name;
+        this.price = price;
+        this.idCategory = idCategory;
+    }
+
+    
+}
---
+++ model/cart.js
@@ -0,0 +1,6 @@
+class cart{
+    constructor(articles, total){
+        this.articles = articles;
+        this.total = total;
+    }
+}
