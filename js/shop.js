b276d7ee45647cd22310ed61f97cf5809bd54716
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-22 13:43:50 +0100

    Modify css to better rendering

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,8 @@
 const categoryContainer = document.getElementById('category');
 const categoryTitle = document.getElementById('category-title');
 const articleContainer = document.getElementById('articles');
-const cartContainer = document.getElementById('cart-container')
+const cartContainer = document.getElementById('cart-container');
+const orderButton = document.getElementById('order');
 
 let articles = [];
 let categories = [];
@@ -76,9 +77,11 @@ function cartEvent(){
             cartContainer.setAttribute('class', "invisible");
         }
     }, "500");
-
-
 }
+
+orderButton.addEventListener('click', () => {
+    order();
+});
 
 /**
  * Création des catégories
@@ -124,14 +127,16 @@ function createArticle(){
 function showArticles(){
     articles.forEach(article => {
         let card = document.createElement('div');
-        card.setAttribute('class', "mt-4 flex flex-col justify-between");
-        card.innerHTML += `<span class="text-sm text-gray-700">${article.name}</span>`;
-        card.innerHTML += `<p class="text-sm font-medium text-gray-900">${article.price}€</p>`;
-        card.innerHTML += `<button type="button" id="${article.name}" class="rounded-md bg-blue-400 px-3 py-2 flex justify-end">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                </svg>
-                           </button>`;
+        card.setAttribute('class', "mt-4 flex flex-col justify-between bg-white rounded-xl shadow-lg");
+        card.innerHTML += `<span class="my-4 mx-2 font-semibold text-sm text-gray-700">${article.name}</span>`;
+        card.innerHTML += `<p class="my-2 mx-2 text-sm font-medium text-gray-900">${article.price} €</p>`;
+        card.innerHTML += `<div class="flex justify-end">
+                                <button type="button" id="${article.name}" class="w-10 rounded-xl bg-blue-400 px-3 py-2 flex items-end justify-end">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                    </svg>
+                                </button>
+                           </div>`;
         articleContainer.appendChild(card);
     })
 }
@@ -150,14 +155,14 @@ function showFilteredArticle(categoryId){
             card.setAttribute('class', "mt-4 flex flex-col justify-between");
             card.innerHTML += `<span class="text-sm text-gray-700">${article.name}</span>`;
             card.innerHTML += `<p class="text-sm font-medium text-gray-900">${article.price}€</p>`;
-            card.innerHTML += `<button type="button" id="${article.name}" class="rounded-md bg-blue-400 px-3 py-2 flex justify-end">
+            card.innerHTML += `<button type="button" id="${article.name}" class="w-10 rounded-xl bg-blue-400 px-3 py-2 flex items-end justify-items-end">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                     </svg>
                                 </button>`;
             articleContainer.appendChild(card);
         
-    })
+    });
 }
 
 /**
@@ -166,15 +171,15 @@ function showFilteredArticle(categoryId){
 function createCart(){
     let cartDiv = document.createElement('div');
     cartDiv.setAttribute('class', "flex flex-col justify-center min-h-screen");
-    cartDiv.innerHTML += `<div class="bg-gray-100 rounded-lg shadow-lg p-6">
+    cartDiv.innerHTML += `<div class="bg-gray-100 rounded-lg shadow-lg p-6 min-h-screen">
                                 <h1 class="font-bold">Panier :</h1>
-                                <div id="cart-content" class="flex flex-col mb-4"></div>
+                                <div id="cart-content" class="flex flex-col my-4 mb-4"></div>
                                 <div class="flex justify-between items-center">
                                     <span class="font-bold">Total :</span>
                                     <span id="total" class="font-bold"></span>
                                 </div>
                                 <div>
-                                    <button class="bg-blue-400 text-white font-bold py-2 px-4 rounded">Passer commande</button>
+                                    <button id="order" class="bg-blue-400 text-white font-bold my-4 py-2 px-4 rounded">Passer commande</button>
                                 </div>                 
                           </div>`;
     cartContainer.appendChild(cartDiv);
@@ -209,23 +214,23 @@ function showCart(){
     cartContent.innerHTML = '';
     cart.forEach(article => {
         let content = document.getElementById('cart-content');
-        content.innerHTML += `<div class="flex flex-col">
+        content.innerHTML += `<div class="flex flex-col my-4">
                                 <span class="">${article.name}</span>
                                 <span class="">${article.price}€</span>
-                                <div class="flex flex-row">
-                                    <button type="button" id="minus${article.id}" class="rounded-md bg-blue-400 px-3 py-2 flex justify-end">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <div class="flex flex-row w-10">
+                                    <button type="button" id="minus${article.id}" class="w-10 rounded-md bg-gray-400 px-2 py-2 flex justify-end">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
                                         </svg>                          
                                     </button>
-                                    <input id="quantity" type="text" value="${article.quantity}" class="w-4 text-center">
-                                    <button type="button" id="plus${article.id}" class="rounded-md bg-blue-400 px-3 py-2 flex justify-end">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <input id="quantity" type="text" value="${article.quantity}" class="w-10 text-center">
+                                    <button type="button" id="plus${article.id}" class="w-10 rounded-md bg-gray-400 px-2 py-2 flex justify-end">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                         </svg>
                                     </button>
-                                    <button type="button" id="delete${article.id}" class="rounded-md bg-blue-400 px-3 py-2 flex justify-end">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <button type="button" id="delete${article.id}" class="w-10 rounded-md bg-pink-400 mx-4 px-2 py-2 flex justify-end">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
                                     </button>
@@ -238,4 +243,8 @@ function showCart(){
 
     let total = document.getElementById('total');
     total.innerHTML = `${totalCart()}`;
+}
+
+function order(){
+    
 }
