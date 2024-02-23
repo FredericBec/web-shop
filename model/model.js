a1f3040659491512da0c41d1df0ab66f171200c4
Frederic Bec <bec.frederic90@outlook.fr>
2024-02-23 13:40:45 +0100

    add images for articles

--- model/model.js
+++ model/model.js
@@ -1,9 +1,10 @@
 class Article{
-    constructor(id, name, price, quantity, idCategory){
+    constructor(id, name, price, quantity, img, idCategory){
         this.id =id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.img = img;
         this.idCategory = idCategory;
     }
 }
